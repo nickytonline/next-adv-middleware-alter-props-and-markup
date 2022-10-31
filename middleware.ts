4596ecbe4bf04ff71efb5b8078afa98be40b6d5a
Nick Taylor <nick@iamdeveloper.com>
2022-10-31 15:21:19 -0400

    chore: initial commit

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+import { MiddlewareRequest, NextDataTransform } from '@netlify/next';
+
+export async function middleware(req: NextRequest) {
+	const request = new MiddlewareRequest(req);
+
+	// Unlike NextResponse.next(), this actually sends the request to the origin
+	const response = await request.next();
+
+	response.transformData((data) => {
+		data.prices = [
+			{ id: 1, price: 20 },
+			{ id: 2, price: 30 },
+			{ id: 3, price: 78 },
+		];
+
+		response.replaceText('li:first-of-type', data.prices[0].price);
+
+		return data;
+	});
+
+	return response;
+}
+
+export const config = {
+	matcher: ['/:all*'],
+};
